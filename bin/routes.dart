50382f7d08adc7cdb16129157a9408a64c4c6db3
John Fedoruk <johnny@johnfedoruk.ca>
2018-08-01 20:09:05 -0500

    Bound HelloController's hello method to GET /hello in routes file

--- bin/routes.dart
+++ bin/routes.dart
@@ -4,7 +4,9 @@ import 'system/verb.dart';
 
 // Controller imports
 import 'controllers/ping.controller.dart';
+import 'controllers/hello.controller.dart';
 
 List<Route> routes = [
   new Route('/ping', Verb.GET, PingController, 'ping'),
+  new Route('/hello', Verb.GET, HelloController, 'hello'),
 ];
