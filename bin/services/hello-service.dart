185922ecac3d8796ecb63ae875770262401b0059
John Fedoruk <johnny@johnfedoruk.ca>
2018-08-01 20:07:24 -0500

    Created a simple service example: HelloService

---
+++ bin/services/hello-service.dart
@@ -0,0 +1,14 @@
+class HelloService {
+  String get html {
+    return """
+      <html>
+        <head>
+          <title>Dart Server</title>
+        </head>
+        <body>
+          <h2>Hello, world</h2>
+        </body>
+      </html>
+      """;
+    }
+}
