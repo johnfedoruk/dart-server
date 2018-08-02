59e7cbab2e5643446c9c80b0e6ea48d1c45f463b
John Fedoruk <johnny@johnfedoruk.ca>
2018-08-01 20:07:03 -0500

    Bound PingController's ping method to GET /ping in routes file

--- bin/routes.dart
+++ bin/routes.dart
@@ -1,7 +1,10 @@
 // System imports
 import 'system/route.dart';
+import 'system/verb.dart';
 
 // Controller imports
+import 'controllers/ping.controller.dart';
 
 List<Route> routes = [
+  new Route('/ping', Verb.GET, PingController, 'ping'),
 ];
