739ad15b8cd70b3db318247d562e64bd9ec4cb1c
John Fedoruk <johnny@johnfedoruk.ca>
2018-11-30 20:21:01 -0600

    Changed LOOPBACK_IP_V4 to ANY_IP_V4 in Server class' run() method

--- bin/system/server.dart
+++ bin/system/server.dart
@@ -7,7 +7,7 @@ import 'router.dart';
 class Server {
   Future run(int port) async {
     HttpServer server =
-        await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port);
+        await HttpServer.bind(InternetAddress.ANY_IP_V4, port);
     print('Listening on localhost:${port}');
     Router router = new Router(server, routes);
     await router.start();
